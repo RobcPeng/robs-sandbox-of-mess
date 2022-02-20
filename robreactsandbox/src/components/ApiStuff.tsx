a65a59ce161bce306aae47222ed889e1e62b6cff
peng <robertjustinianpeng@gmail.com>
2022-02-19 18:34:12 -0700

    tnx graf

--- robreactsandbox/src/components/ApiStuff.tsx
+++ robreactsandbox/src/components/ApiStuff.tsx
@@ -3,15 +3,16 @@ import { useEffect, useState } from "react";
 export function ApiSandbox() {
   const [apiStuff, setApiStuff] = useState("");
 
-  const randomApi = () => {
-    return fetch("https://randomuser.me/api")
-      .then((val) => {
-        val.json();
-      })
-      .then((d) => {
-        return setApiStuff(JSON.stringify(d) || "");
-      })
-      .catch((err) => console.log(err));
+  const randomApi = async () => {
+    // return fetch("https://randomuser.me/api")
+    //   .then((val) => {
+    //     val.json().then((d) => setApiStuff(JSON.stringify(d)));
+    //   })
+    //   .catch((err) => console.log(err));
+    const response = await fetch("https://randomuser.me/api");
+    setApiStuff(JSON.stringify(await response.json()) || "");
+
+    //Both ways work - fetch returns a Response object/interface that .json() breaks out, .json is a promise so await needs to be triggered here
   };
 
   useEffect(() => {
