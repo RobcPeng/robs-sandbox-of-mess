9cbdb627440b5037a4f5c5de4a674d152bfc9535
peng <robertjustinianpeng@gmail.com>
2022-03-03 14:37:19 -0700

    backend poc

--- nodeBackend/dist/index.js
+++ nodeBackend/dist/index.js
@@ -16,7 +16,17 @@
   \***********************/
 /***/ (function(__unused_webpack_module, exports, __webpack_require__) {
 
-eval("\nvar __importDefault = (this && this.__importDefault) || function (mod) {\n    return (mod && mod.__esModule) ? mod : { \"default\": mod };\n};\nObject.defineProperty(exports, \"__esModule\", ({ value: true }));\nconst express_1 = __importDefault(__webpack_require__(/*! express */ \"express\"));\n(__webpack_require__(/*! dotenv */ \"dotenv\").config)();\nconst app = (0, express_1.default)();\nconst port = \"3334\";\napp.get(\"/initial\", (req, res) => {\n    res.status(200).json({ hi: \"world\" });\n});\napp.listen(port, () => {\n    console.log(`this is running on ${port}`);\n});\n\n\n//# sourceURL=webpack://nodebackend/./src/server.ts?");
+eval("\nvar __importDefault = (this && this.__importDefault) || function (mod) {\n    return (mod && mod.__esModule) ? mod : { \"default\": mod };\n};\nObject.defineProperty(exports, \"__esModule\", ({ value: true }));\nconst cors_1 = __importDefault(__webpack_require__(/*! cors */ \"cors\"));\nconst express_1 = __importDefault(__webpack_require__(/*! express */ \"express\"));\n(__webpack_require__(/*! dotenv */ \"dotenv\").config)();\nconst app = (0, express_1.default)();\nconst port = \"3334\";\n// Enable cors for preflight\napp.options(\"*\", (0, cors_1.default)());\n// Whitelist all routes with cors\napp.use((0, cors_1.default)());\n// Use express json\napp.use(express_1.default.json());\napp.get(\"/initial\", (req, res) => {\n    res.status(200).json({ hi: \"world\" });\n});\napp.listen(port, () => {\n    console.log(`this is running on ${port}`);\n});\n\n\n//# sourceURL=webpack://nodebackend/./src/server.ts?");
+
+/***/ }),
+
+/***/ "cors":
+/*!***********************!*\
+  !*** external "cors" ***!
+  \***********************/
+/***/ ((module) => {
+
+module.exports = require("cors");
 
 /***/ }),
 
--- nodeBackend/src/server.ts
+++ nodeBackend/src/server.ts
@@ -1,8 +1,17 @@
+import cors from "cors";
 import express from "express";
 require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT;
+// Enable cors for preflight
+app.options("*", cors());
+
+// Whitelist all routes with cors
+app.use(cors());
+
+// Use express json
+app.use(express.json());
 
 app.get("/initial", (req, res) => {
   res.status(200).json({ hi: "world" });
